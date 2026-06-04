00d160b60390fec5a3d1c7564d101487d7f1af7e
Anna But <ann28876@gmail.com>
2026-06-04 18:30:44 +0300

    Add delivery service logic

--- backend/app/models/delivery.py
+++ backend/app/models/delivery.py
@@ -1,20 +1,22 @@
+from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import String, ForeignKey, Text
+from sqlalchemy import String, ForeignKey, Text, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database import Base
 
 class Delivery(Base):
-    __tablename__ = "delivery"
+    __tablename__ = "deliveries"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[int] = mapped_column("delivery_id", primary_key=True, index=True)
     city: Mapped[str] = mapped_column(String(50))
     street: Mapped[str]= mapped_column(String(100))
     house_number: Mapped[str] = mapped_column(String(20))
     flat_number: Mapped[Optional[str]] = mapped_column(String(20))
+    delivery_datetime: Mapped[datetime] = mapped_column(DateTime, nullable=False)
 
     employee_id: Mapped[int] = mapped_column(ForeignKey(
-        "EmployeeBase.id", ondelete="CASCADE"))
+        "employees.employee_id", ondelete="CASCADE"))
     order_id: Mapped[int] = mapped_column(ForeignKey(
-        "OrderBase.id", ondelete="CASCADE"))
+        "orders.order_id", ondelete="CASCADE"))
