267422ba067eea34e5f45747c90fe89c20cd8d3f
Anna But <ann28876@gmail.com>
2026-06-05 12:40:12 +0300

    Add validation schemas for User

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,35 +1,42 @@
+from enum import Enum
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, field_validator, ConfigDict
+from pydantic import BaseModel, EmailStr, field_validator, ConfigDict, Field
 
 
 class UserBase(BaseModel):
     name: str
     surname: str
     patronymic: Optional[str] = None
-
-    password: str
-
-    @field_validator('password')
-    def check_password(cls, value):
-        if len(value) < 6:
-            raise ValueError(
-                'Пароль має містити мінімум 6 символів'
-            )
-        return value
-
     email: EmailStr
     phone_number: str
 
     model_config = ConfigDict(from_attributes=True)
 
 class UserRegister(UserBase):
-    pass
+    password: str = Field(min_length=4)
+    role: str
 
-class UserLogin(BaseModel):
-    email: EmailStr
+class UserInDB(UserBase):
     password: str
+    role: str
 
-class ClientRead(UserBase):
-    client_id: int
+class UserRead(UserBase):
+    id: int
+    role: str
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    surname: Optional[str] = None
+    email: Optional[EmailStr] = None
+    phone_number: Optional[str] = Field(None, max_length=15)
+    password: Optional[str] = Field(None, min_length=4)
+    role: Optional[str] = None
+
     model_config = ConfigDict(from_attributes=True)
+
+class UserRole(str, Enum):
+    USER = "клієнт"
+    ADMIN = "адміністратор"
+    COURIER = "кур'єр"
+    CHIEF = "керівник"
