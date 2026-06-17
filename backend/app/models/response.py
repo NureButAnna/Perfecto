23d21c2f54cf35eb10563caf78142361f5d2b460
NureButAnna <@email>
2026-06-17 11:05:52 +0300

    Rename model

--- backend/app/models/response.py
+++ backend/app/models/response.py
@@ -3,9 +3,9 @@ from sqlalchemy.testing.schema import mapped_column
 from src.database import Base
 
 class Response(Base):
-    __tablename__ = "response"
+    __tablename__ = "responses"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[int] = mapped_column("response_id", primary_key=True, index=True)
     text: Mapped[Optional[str]] = mapped_column(Text)
     date: Mapped[datetime.datetime] = mapped_column(
         server_default=text("TIMEZONE('utc', now()"))
