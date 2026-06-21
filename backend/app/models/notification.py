56945b21d748618a0fb0986cbcc93a39d43c0e29
NureButAnna <@email>
2026-06-21 14:50:19 +0300

    Refactor notification model

--- backend/app/models/notification.py
+++ backend/app/models/notification.py
@@ -5,13 +5,14 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
 class Notification(Base):
-    __tablename__ = "notification"
+    __tablename__ = "notifications"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[int] = mapped_column("notification_id",
+                                    primary_key=True, index=True)
     text: Mapped[str] = mapped_column(Text)
     status: Mapped[str] = mapped_column(String(50))
     date: Mapped[datetime.datetime] = mapped_column(
         server_default=text("TIMEZONE('utc', now()"))
 
-    client_id: Mapped[int] = mapped_column(ForeignKey(
-        "ClientBase.id", ondelete="CASCADE"))
+    user_id: Mapped[int] = mapped_column(ForeignKey(
+        "users.user_id", ondelete="CASCADE"))
