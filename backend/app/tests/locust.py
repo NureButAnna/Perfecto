75f59a22a38b8af46e8d5562105a7379791e7e06
NureButAnna <@email>
2026-06-25 00:26:26 +0300

    `Add locust file for load testing

---
+++ backend/app/tests/locust.py
@@ -0,0 +1,30 @@
+from locust import HttpUser, task, between
+
+class ServicesLoadTest(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def get_all_services(self):
+        self.client.get("/services/")
+
+    @task
+    def get_service_by_id(self):
+        self.client.get("/services/1")
+
+
+class AuthLoadTest(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def login_valid(self):
+        self.client.post("/auth/login", json={
+            "email": "delivery@gmail.com",
+            "password": "111111"
+        })
+
+    @task
+    def login_invalid(self):
+        self.client.post("/auth/login", json={
+            "email": "wrong@gmail.com",
+            "password": "wrongpassword"
+        })
