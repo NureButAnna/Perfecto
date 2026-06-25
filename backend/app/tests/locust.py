fd36297961cb294c79d4e1fee34e9ea596c8c586
NureButAnna <@email>
2026-06-25 09:43:06 +0300

    Refactor locust file

--- backend/app/tests/locust.py
+++ backend/app/tests/locust.py
@@ -10,21 +10,3 @@ class ServicesLoadTest(HttpUser):
     @task
     def get_service_by_id(self):
         self.client.get("/services/1")
-
-
-class AuthLoadTest(HttpUser):
-    wait_time = between(1, 3)
-
-    @task
-    def login_valid(self):
-        self.client.post("/auth/login", data={
-            "username": "delivery@gmail.com",
-            "password": "111111"
-        })
-
-    @task
-    def login_invalid(self):
-        self.client.post("/auth/login", data={
-            "username": "wrong@gmail.com",
-            "password": "wrongpassword"
-        })
